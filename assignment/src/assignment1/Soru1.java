9bdd46d6d775bd2a0b2ba200f70a23867b38f49a
Jasi <Jasi@Asi1987>
2022-12-08 18:44:17 +0100

    ilk odevim

---
+++ assignment/src/assignment1/Soru1.java
@@ -0,0 +1,58 @@
+package assignment1;
+
+public class Soru1 {
+public static void main(String[]args) {
+		
+		// primitiv variable 
+		// byte short int long float double char boolean = 8 adet
+		
+		byte a= 3; System.out.println(a);
+		byte b=34;System.out.println(b);
+		byte c=127;System.out.println(c);
+		
+		
+		short d= 17586; System.out.println(d);
+		short e=9999;System.out.println(e);
+		short f=-32000;System.out.println(f);
+		
+		
+		
+		
+		int g= 2147483646; System.out.println(g);
+		int h=-786637695;System.out.println(h);
+		int k=-2147483648;System.out.println(k);
+		
+		
+		
+		
+		long m= 922337203685477580l; System.out.println(m);
+		long n=-92233720368547758l;System.out.println(n);
+	    long omega=-21;System.out.println(omega);
+	    
+	 
+		
+		float aki=0.39999f; System.out.println(aki);
+		float maki=1.5748f;System.out.println(maki);
+	    float taki=2.748f;System.out.println(taki);
+	    
+	    
+		
+		double yeni=1.7899999; System.out.println(yeni);
+		double ani=-3.55;System.out.println(ani);
+	    double tani=1.847575;System.out.println(tani);
+	    
+	
+	    
+	    boolean fan=true;System.out.println(fan);
+	    boolean kan=false;System.out.println(kan);
+	    
+	    
+	    char un = 'a';System.out.println(un);
+	    char ban= '&';System.out.println(ban);
+	    char van= '1';System.out.println(van);
+	    
+	   
+	}
+
+}
+
